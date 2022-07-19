d9be5d931dd53f6680c3907476e6352b0adfa99c
lemans77 <oleg.boiechko@gmail.com>
2022-07-19 15:25:57 +0300

    rename website url

--- config/app.php
+++ config/app.php
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://testsite.local'),
+    'url' => env('APP_URL', 'http://testsite.laravel'),
 
     'asset_url' => env('ASSET_URL', null),
 
